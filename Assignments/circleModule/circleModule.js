3c8766e9349d12abbf16077193f63230e5d3d0de
Muharrem Aslan <muharrem5352@gmail.com>
2024-10-12 14:26:26 +0300

    circleModuleExporting
    Create a circle.js file that contains two functions: circleArea (to calculate the area of a circle) and circleCircumference. These functions should be exported using module.exports.
    
    Then, create an index.js file where you import these functions using require and object destructuring. Set the radius (r) to 5 and display the results  in the console.

---
+++ Assignments/circleModule/circleModule.js
@@ -0,0 +1,13 @@
+function circleArea(radius){
+
+    return Math.PI * radius*radius;
+}
+
+function circleCircumference(radius){
+        return 2 * Math.PI * radius;
+}
+
+module.exports ={
+    circleArea,
+    circleCircumference
+}
---
+++ Assignments/circleModule/index.js
@@ -0,0 +1,5 @@
+const { circleArea,circleCircumference } = require("./circleModule");
+
+
+console.log("Area: ",circleArea(5))
+console.log("Circumference: ",circleCircumference(5))
