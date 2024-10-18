7fe3e72e5ed5e467b02b0039d973b5208bae5211
Muharrem Aslan <muharrem5352@gmail.com>
2024-10-19 00:28:58 +0300

    CleanBlog app added
    The project is a small-scale blog page written as a warm-up and to review basic topics related to Node.js.
    Workspace created.

---
+++ Apps/CleanBlog/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const blog = { id: 1, title: "Blog title", description: "Blog description" };
+app.get('/test', (req, res) => {
+  res.send(blog);
+})
+
+app.listen(port, () => {
+  console.log(`Sunucu  ${port}'da dinleniyor.`)
+})
