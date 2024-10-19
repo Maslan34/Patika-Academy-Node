d20084d4f90c7aeaa47f54f589f75131f5890c80
Muharrem Aslan <muharrem5352@gmail.com>
2024-10-20 00:39:06 +0300

    Content preparation
    Create a public folder and place the static files inside it.
    Write the appropriate middleware function to register the public folder with the application.
    Install the EJS module.
    Specify in the application that we will use the EJS module.
    Create a views folder and convert all .html files within it into .ejs files.
    Create the partials folder structure.
    Set up the necessary routes and update the links in the _navigation.ejs file.

--- Apps/CleanBlog/app.js
+++ Apps/CleanBlog/app.js
@@ -1,13 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
 
+const port = 3000;
 
-const blog = { id: 1, title: "Blog title", description: "Blog description" };
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs'); // BURASI ÖNCELİKLE VİEWS KLASÖRÜNE BAKAR.
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/about', (req, res) => res.render('about'));
+app.get('/add', (req, res) => res.render('add_post'));
+
+const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
 app.get('/test', (req, res) => {
   res.send(blog);
-})
+});
+
+app.get('/test', (req, res) => {
+  res.send(blog);
+});
 
 app.listen(port, () => {
-  console.log(`Sunucu  ${port}'da dinleniyor.`)
-})
+  console.log(`Sunucu  ${port}'da dinleniyor.`);
+});
