948540f2720199938f430637a52d154a58cd51c2
Muharrem Aslan <muharrem5352@gmail.com>
2025-02-10 11:38:38 +0300

    Mail & Dotenv
    The Nodemailer package was used to send an email to the user via Gmail by collecting user information from the contact page. An app password was obtained to send emails through Gmail, and this password was stored as an environment variable in dotenv to protect sensitive information.

--- Apps/Misfit/controllers/pageController.js
+++ Apps/Misfit/controllers/pageController.js
@@ -1,3 +1,16 @@
+const nodemailer = require("nodemailer");
+require('dotenv').config()
+
+// Gmail SMTP Configuration
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_MAIL,  
+    pass: process.env.GMAIL_PASSWORD,  
+  },
+});
+
+
 const goIndexPage = (req, res) => {
   res.render("index", { pageName: "index" });
 };
@@ -13,10 +26,40 @@ const goSignUpPage = (req, res) => {
   res.render("signUp", { pageName: "signUp" });
 };
 
+const goContactPage = (req, res) => {
+  res.render("contact", { pageName: "contact" });
+};
+
+const contactUs = (req, res) => {
+
+// Mail Content
+const mailOptions = {
+  from: process.env.GMAIL_MAIL,
+  to: req.body.email,
+  subject:  `About Your ${req.body.subject} Mail`,
+  text: `Hi ${req.body.name}! We Reacieved your email, we will  message you as soon as possible`,
+};
+  // Sending Mail
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log("Error Occured: ", error);
+  } else {
+    console.log("Mail Sent: ", info.response);
+  }
+});
+
+  res.redirect("/")
+};
+
+
+
+
 
 module.exports = {
   goIndexPage,
   goAboutPage,
   goLoginPage,
-  goSignUpPage
+  goSignUpPage,
+  goContactPage,
+  contactUs
 };
--- Apps/Misfit/routes/pageRoute.js
+++ Apps/Misfit/routes/pageRoute.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.route("/").get(pageController.goIndexPage);
 router.route("/about").get(pageController.goAboutPage);
+router.route("/contact").get(pageController.goContactPage);
+router.route("/contact").post(pageController.contactUs);
 router.route("/login").get(redirectMiddleware,pageController.goLoginPage);
 router.route("/signUp").get(redirectMiddleware,pageController.goSignUpPage);
 //router.route("/trainers").get(pageController.goTrainersPage);
